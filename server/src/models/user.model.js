3c3813b19d5e92f7e2f61fd5f4be8deb3f9808c2
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-20 17:20:04 +0530

    Refactor User Schema: Add methods, Change age to Number, add default profileUrl, and add gender validation

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,46 +1,74 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minLength: 2,
-    maxLength: 20,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "others"],
+      message: `{VALUE} is not a valid gender type`,
+    },
+    profileUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+    },
+    about: {
+      type: String,
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-    minLength: 2,
-    maxLength: 20,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: String,
-  },
-  gender: {
-    type: String,
-    enum: ["male", "female", "others"]
-  },
-  profileUrl: {
-    type: String,
-  },
-  about: {
-    type: String,
-  },
-  skills: {
-    type: [String],
-    minLength: 10,
-  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateToken = async function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+  return token;
+};
 
 module.exports = mongoose.model("User", userSchema);
