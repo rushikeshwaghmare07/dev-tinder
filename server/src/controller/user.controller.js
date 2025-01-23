6acde5270a5c0db947bdff760fc520dbd7472521
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-23 22:05:27 +0530

    Implement Get connections functionality

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -7,7 +7,7 @@ const getReceivedConnectionRequests = async (req, res) => {
     // Find connection requests sent to the logged-in user
     const user = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
-      status: "interested"
+      status: "interested",
     }).populate({
       path: "fromUserId",
       select: "firstName lastName profileUrl age gender about skills",
@@ -21,11 +21,45 @@ const getReceivedConnectionRequests = async (req, res) => {
   } catch (error) {
     return res.status(500).json({
       success: false,
-      message: error?.message || "Failed to fetch connection requests. Please try again later.",
+      message:
+        error?.message ||
+        "Failed to fetch connection requests. Please try again later.",
+    });
+  }
+};
+
+const getConnections = async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionData = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate({
+      path: "fromUserId",
+      select: "firstName lastName profileUrl age gender about skills",
+    });
+
+    const data = connectionData.map((row) => row.fromUserId);
+
+    return res.status(200).json({
+      success: true,
+      data: data,
+      message: "All connection fetched successfully.",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message:
+        error?.message ||
+        "Failed to fetch connections. please try again later.",
     });
   }
 };
 
 module.exports = {
   getReceivedConnectionRequests,
+  getConnections,
 };
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth.middleware");
-const { getReceivedConnectionRequests } = require("../controller/user.controller");
+const { getReceivedConnectionRequests, getConnections } = require("../controller/user.controller");
 
 const router = express.Router();
 
 router.get("/connections/requests/received", userAuth, getReceivedConnectionRequests);
+router.get("/connections/show", userAuth, getConnections);
 
 module.exports = router;
