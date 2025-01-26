93c6650d54f871299a4f0d77f95e32d858db533c
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-26 19:48:41 +0530

    feat: Install Tailwind CSS and Daisy UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+      </button>
+    </div>
+  );
+};
 
-export default App
+export default App;
