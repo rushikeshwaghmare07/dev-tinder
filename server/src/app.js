5a622388c6d00ba702116503030864bf41fa63ac
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-27 20:08:17 +0530

    feat: Add CORS origin

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,12 @@ const userRoute = require("./routes/user.routes.js");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
