f3d49c4bdac2c90714a11347cdb9a8f06f86791b
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-27 23:01:14 +0530

    feat: Update user login controller

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -91,6 +91,7 @@ const userLogin = async (req, res) => {
 
     return res.status(200).json({
       success: true,
+      date: user,
       message: "User logged in successfully.",
     });
   } catch (error) {
