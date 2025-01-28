2ae3b9756236d6b328182e301501216d4571dadb
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-28 19:44:35 +0530

    Implement user authentication and state management in Body component

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
-import Navbar from './Navbar'
-import { Outlet } from "react-router-dom"
+import React, { useEffect } from "react";
+import Navbar from "./Navbar";
+import { Outlet, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BACKEND_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../features/user/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    if (userData) return;
+    
+    try {
+      const res = await axios.get(`${BACKEND_URL}/api/profile/view`, {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (error) {
+      navigate("/login")
+      console.error("Fetch User Error: ", error.response?.data || error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
