fc50dcb6654c8ea30e8c73f2d087bf6930909360
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-29 18:02:04 +0530

    Implement Feed Page functionality

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { BACKEND_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../features/feed/feedSlice";
+import UserCard from "./UserCard";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
 
-export default Feed
+  const getFeed = async () => {
+    try {
+      const { data } = await axios.get(`${BACKEND_URL}/api/user/feed`, {
+        withCredentials: true,
+      });
+
+      dispatch(addFeed(data?.data));
+    } catch (error) {
+      console.log(
+        error.response?.data?.message || "An error occurred. Please try again."
+      );
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  return (
+    <div className="flex justify-center mt-2">
+      {feed.length > 0 ? <UserCard user={feed[0]} /> : <h1>No users found</h1>}
+    </div>
+  );
+};
+
+export default Feed;
