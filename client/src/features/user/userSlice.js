9fab600cf9105ba7bf799603aa57158d542e4f1b
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-01 22:56:16 +0530

    Update User slice

--- client/src/features/user/userSlice.js
+++ client/src/features/user/userSlice.js
@@ -2,12 +2,12 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
   name: "user",
-  initialState: null,
+  initialState: {},
   reducers: {
     addUser: (state, action) => {
       return action.payload;
     },
-    removeUser: (state, action) => {
+    removeUser: () => {
       return null;
     },
   },
