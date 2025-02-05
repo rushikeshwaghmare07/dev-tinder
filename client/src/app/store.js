9249325beedd9222692ec4501b18a4885ed3dc73
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-05 21:58:10 +0530

    Update feedSlice and store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../features/user/userSlice.js";
-import feedReducer from "../features/feed/feedSlice.js";
+import userReducer from "./features/user/userSlice.js";
+import feedReducer from "./features/feed/feedSlice.js";
 
 export const store = configureStore({
   reducer: {
