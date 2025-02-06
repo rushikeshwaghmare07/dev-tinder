f59f1617eb13393b386448bfa7b9ea39a04772d1
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-06 18:39:58 +0530

    Add request slice and update it in a store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice.js";
 import feedReducer from "./features/feed/feedSlice.js";
-import connectionReducer from "./features/connections/connectionSlice.js"
+import connectionReducer from "./features/connections/connectionSlice.js";
+import requestReducer from "./features/requests/requestSlice.js";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     feed: feedReducer,
-    connections: connectionReducer
+    connection: connectionReducer,
+    requests: requestReducer,
   },
 });
