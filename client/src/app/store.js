b3c72dceac976f3c0067420a6b31576dea0d1b3b
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-06 17:17:23 +0530

    Add connection slice and update it in a store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice.js";
 import feedReducer from "./features/feed/feedSlice.js";
+import connectionReducer from "./features/connections/connectionSlice.js"
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     feed: feedReducer,
+    connections: connectionReducer
   },
 });
