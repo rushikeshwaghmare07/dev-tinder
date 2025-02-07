6b335f9f34f8f8c0e251f8e0762126ecfe3889c8
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-07 20:15:28 +0530

    Update: add remove request slice

--- client/src/app/features/requests/requestSlice.js
+++ client/src/app/features/requests/requestSlice.js
@@ -5,9 +5,13 @@ const requestSlice = createSlice({
   initialState: null,
   reducers: {
     addRequests: (state, action) => action.payload,
+    removeRequest: (state, action) => {
+      const newArray = state.filter((req) => req._id !== action.payload);
+      return newArray;
+    },
   },
 });
 
-export const { addRequests } = requestSlice.actions;
+export const { addRequests, removeRequest } = requestSlice.actions;
 
 export default requestSlice.reducer;
